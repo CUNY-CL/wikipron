f86935e8a3d8f197d761043fa8270bd2fdb11eab
jacksonllee <jacksonlunlee@gmail.com>
2019-06-25 18:08:29 -0500

    Revert 1509865 'ENH check CLI args only once in _print_data'

--- languages/eng_wik/scrape.py
+++ languages/eng_wik/scrape.py
@@ -43,25 +43,6 @@ def _yield_phn(request):
 
 
 def _print_data(data, args):
-    def process_pron_factory():
-        scrubbers = [
-            fn
-            for cli_flag, fn in [
-                ('no_stress', lambda x: x.replace('ˈ', '').replace('ˌ', '')),
-                ('no_syllable_boundaries', lambda x: x.replace('.', ''))
-            ]
-            if getattr(args, cli_flag)
-        ]
-
-        def wrapper(pron):
-            for scrubber in scrubbers:
-                pron = scrubber(pron)
-            return pron
-
-        return wrapper
-
-    process_pron = process_pron_factory()
-
     session = requests_html.HTMLSession()
     for member in data["query"]["categorymembers"]:
         word = member["title"]
@@ -82,8 +63,11 @@ def _print_data(data, args):
             pron = pron.replace("(", "").replace(")", "")
             # Skips examples with a space in the pron.
             if " " in pron:
-                continue
-            pron = process_pron(pron)
+                break
+            if args.no_stress:
+                pron = pron.replace('ˈ', '').replace('ˌ', '')
+            if args.no_syllable_boundaries:
+                pron = pron.replace('.', '')
             print(f"{word}\t{pron}")
 
 
