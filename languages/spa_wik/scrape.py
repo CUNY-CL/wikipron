381d7fe8203a5c376c297b772c0609ceff39ab2e
Elizabeth Garza <garza.elizabeth9@gmail.com>
2019-07-05 14:30:54 -0400

    final spa edits to maximize readability

--- languages/spa_wik/scrape.py
+++ languages/spa_wik/scrape.py
@@ -16,37 +16,9 @@ CONTINUE_TEMPLATE = string.Template(INITIAL_QUERY + "&cmcontinue=$cmcontinue")
 # Selects the content on the page.
 PAGE_TEMPLATE = string.Template("https://en.wiktionary.org/wiki/$word")
 
-
-<<<<<<< HEAD
-LI_SELECTOR_for_Latin_American_phonemes_v_1 = """
-//li[last()][sup[a[@title = "Appendix:Spanish pronunciation"]] and span[@class = "IPA"]]
-"""
-
-""" 
-LI_SELECTOR..._v_2 assumes that Latin American phonemes are the only entries that won't have a hyperlinked description  
-before providing the phoneme.    
-"""
-
-LI_SELECTOR_for_Latin_American_phonemes_v_2 = """
-//li[sup[a[@title = "Appendix:Spanish pronunciation"]] and span[@class = "IPA"]
-and 
-count(span[a]) =0]
-"""
-
-"""
-LI_SELECTOR..._v_3 assumes that entries are Latin American if the non-hyperlinked textual description,  
-"Latin America", is provided before the phoneme, or if no description is provided at all.   
-"""
-
-LI_SELECTOR_for_Latin_American_phonemes_v_3 = """
-//li[sup[a[@title = "Appendix:Spanish pronunciation"]] and span[@class = "IPA"]
-and
-(span[@class = "ib-content qualifier-content"][text() = "Latin America"]
-or
-count(span[@class = "ib-content qualifier-content"]) = 0)]
-"""
-
-=======
+# This LI_SELECTOR selects a list element which contains Spanish IPA pronunciation.
+# The pronunciation must either be unlabeled, or labeled with a plain-text description that says, "Latin America".
+# In the former case, we assume the pronunciation is compatible with Latin American Spanish.
 LI_SELECTOR = """
 //li[
 sup[a[@title = "Appendix:Spanish pronunciation"]] 
@@ -60,7 +32,7 @@ count(span[@class = "ib-content qualifier-content"]) = 0
 )
 ]
 """
->>>>>>> 0cf0ce83571a68d8c0fa0f8348f7ebad9623324e
+
 SPAN_SELECTOR = '//span[@class = "IPA"]'
 PHONEMES = r"/(.+?)/"
 
@@ -95,14 +67,10 @@ def _print_data(data, args):
             if " " in pron:
                 continue
             if args.no_stress:
-<<<<<<< HEAD
-                pron = pron.replace('ˈ', '').replace('ˌ', '')
-            print(f"{word.casefold()}\t{pron}")
-=======
-                pron = pron.replace('ˈ', '')
-            print(f"{word}\t{unstressed_pron}")
->>>>>>> 0cf0ce83571a68d8c0fa0f8348f7ebad9623324e
-            
+                pron = pron.replace("ˈ", "").replace("ˌ", "")
+                print(f"{word}\t{pron}")
+
+
 def main(args):
     data = requests.get(INITIAL_QUERY).json()
     _print_data(data, args)
