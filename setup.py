78259c276f348eaaf8fdcf9c43daeddfd85018ae
Elizabeth Garza <garza.elizabeth9@gmail.com>
2019-08-03 15:01:20 -0400

    final formatting edits

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import os
 from setuptools import find_packages, setup
 
 
-_THIS_DIR = os.path.dirname(os.path.realpath('README.md'))
+_THIS_DIR = os.path.dirname(os.path.realpath("README.md"))
 
-with open(os.path.join(_THIS_DIR, 'README.md')) as f:
+with open(os.path.join(_THIS_DIR, "README.md")) as f:
     _LONG_DESCRIPTION = f.read().strip()
 
 
@@ -17,22 +17,22 @@ def main():
         version=__version__,
         description="Scraping Wiktionary data.",
         long_description=_LONG_DESCRIPTION,
-        license='Apache 2.0',
+        license="Apache 2.0",
         packages=find_packages(),
         python_requires=">=3.6",
         zip_safe=False,
         install_requires=["requests", "requests-html", "iso639"],
         entry_points={"console_scripts": ["wikipron = wikipron:main"]},
         classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Development Status :: 3 - Alpha"
-        "Environment :: Console"
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Text Processing :: Linguistic",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Development Status :: 3 - Alpha"
+            "Environment :: Console"
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Topic :: Text Processing :: Linguistic",
         ],
-        )
+    )
 
 
 if __name__ == "__main__":
--- test_wikipron.py
+++ test_wikipron.py
@@ -5,7 +5,13 @@ import tempfile
 
 import pytest
 
-from wikipron import __doc__, _Config, _PHONEMES_REGEX, _PHONES_REGEX, _get_cli_args
+from wikipron import (
+    __doc__,
+    _Config,
+    _PHONEMES_REGEX,
+    _PHONES_REGEX,
+    _get_cli_args,
+)
 
 
 _TERMINAL_COMMAND = "wikipron"
@@ -91,14 +97,12 @@ def test_process_pron(no_stress, no_syllable_boundaries, expected_pron):
         # Input cut_off_date is invalid.
         (True, _DATE_FUTURE, None, None, None),
         (True, "not-a-valid_date", None, None, None),
-
         # Input cut_off_date is valid.
         (False, None, _DATE_RECENT_PAST, "foobar", "foobar"),
         (False, _DATE_TODAY, _DATE_TODAY, "foobar", "foobar"),
         (False, _DATE_TODAY, _DATE_RECENT_PAST, "foobar", "foobar"),
         (False, _DATE_RECENT_PAST, _DATE_DISTANT_PAST, "foobar", "foobar"),
         (False, _DATE_RECENT_PAST, _DATE_TODAY, "foobar", None),
-
         # Now check that filtering works due to the word itself.
         (False, None, _DATE_RECENT_PAST, "a phrase", None),
         (False, None, _DATE_RECENT_PAST, "hyphen-ated", None),
