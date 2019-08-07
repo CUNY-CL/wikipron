b2f7a95cda078bf9cffac9dca4a55601f7b4fed4
jacksonllee <jacksonlunlee@gmail.com>
2019-08-07 10:13:57 -0500

    Ensure setuptools isn't too old

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 import os
-from setuptools import find_packages, setup
+import setuptools
+
+
+if getattr(setuptools, "__version__", "0") < "39":
+    # v36.4.0+ needed to automatically include README.md in packaging
+    # v38.6.0+ needed for long_description_content_type in setup()
+    raise EnvironmentError(
+        "Your setuptools is too old. "
+        "Please run 'pip install --upgrade pip setuptools'."
+    )
 
 
 _THIS_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -12,13 +21,13 @@ __version__ = "0.1.0"
 
 
 def main():
-    setup(
+    setuptools.setup(
         name="wikipron",
         version=__version__,
         description="Scraping Wiktionary data.",
         long_description=_LONG_DESCRIPTION,
         license="Apache 2.0",
-        packages=find_packages(),
+        packages=setuptools.find_packages(),
         python_requires=">=3.6",
         zip_safe=False,
         install_requires=["requests", "requests-html", "iso639"],
