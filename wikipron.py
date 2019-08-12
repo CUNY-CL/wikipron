8f23a9e500b1aad174fc431ce944663cfc21f67f
jacksonllee <jacksonlunlee@gmail.com>
2019-08-12 13:41:52 -0500

    Mark _LANGUAGE_CODES as a place to add more languages as needed

--- wikipron.py
+++ wikipron.py
@@ -52,6 +52,7 @@ _PHONES_REGEX = r"\[(.+?)\]"  # FIXME: it doesn't grab anything now
 # Map from a ISO 639 code to its non-ISO 639 Wiktionary language name.
 # ISO 639-3: https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3.tab  # noqa: E501
 # Wiktionary: https://en.wiktionary.org/wiki/Category:Terms_with_IPA_pronunciation_by_language  # noqa: E501
+# TODO: Expand this as needed to cover additional languages.
 _LANGUAGE_CODES = {
     # Greek. Would have been "Modern Greek (1453-)" in ISO 639.
     "el": "Greek",
