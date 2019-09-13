65b93921623c97b1313b0aacab3025cdb805d892
Lucas <lfeashby@gmail.com>
2019-09-13 19:00:36 -0400

    Added error handling and expanded language_samples to all languages with over 1000 entries

--- languages/wikipron/api_test/api_testing.py
+++ languages/wikipron/api_test/api_testing.py
@@ -1,30 +1,60 @@
 import wikipron
 from language_samples import LANGUAGES
 from datetime import datetime
+from time import sleep
+import requests
+import os
+
+def call_scrape(lang, config, file, retries=0):
+  if retries >= 3:
+    return None
+
+  count = 0
+  try: 
+    for (word, pron) in wikipron.scrape(config):
+      count += 1
+      print(f"{word}\t{pron}", file=file)
+  except requests.exceptions.ReadTimeout as err:
+    print("Timeout error detected during scrape.", lang, err)
+    # Pause execution for 15 min.
+    sleep(900)
+    retries += 1
+    return call_scrape(lang, config, file, retries)
+
+  return count
+
+
 
 def main():
-  # Readme file needs to have a blank line below table headers to ensure this gets written to correct place. 
   readme_file = open("readme_test.md", "a")
 
   for iso639_code in LANGUAGES:
     print('Currently running:', LANGUAGES[iso639_code]["wiktionary_name"], iso639_code, str(datetime.now()))
 
     row = [iso639_code, LANGUAGES[iso639_code]["iso639_name"], LANGUAGES[iso639_code]["wiktionary_name"], str(LANGUAGES[iso639_code]["casefold"])]
-    phonemic_count = 0
-    phonetic_count = 0
-    phonemic_file = open(iso639_code + '.tsv', 'w')
-    phonetic_file = open(iso639_code + '_phonetic' + '.tsv', 'w')
 
-    config = wikipron.Config(key=iso639_code, casefold=LANGUAGES[iso639_code]["casefold"], no_stress=True, no_syllable_boundaries=True)
-    for (word, pron) in wikipron.scrape(config):
-      phonemic_count += 1
-      print(f"{word}\t{pron}", file=phonemic_file)
-
+    phonemic_config = wikipron.Config(key=iso639_code, casefold=LANGUAGES[iso639_code]["casefold"], no_stress=True, no_syllable_boundaries=True)
     phonetic_config = wikipron.Config(key=iso639_code, casefold=LANGUAGES[iso639_code]["casefold"], phonetic=True, no_stress=True, no_syllable_boundaries=True)
-    for (word, pron) in wikipron.scrape(phonetic_config):
-      phonetic_count += 1
-      print(f"{word}\t{pron}", file=phonetic_file)
-      
+
+    phonemic_file = open(iso639_code + ".tsv", "w")
+    phonetic_file = open(iso639_code + "_phonetic" + ".tsv", "w")
+
+    phonemic_count = call_scrape(iso639_code, phonemic_config, phonemic_file)
+    phonetic_count = call_scrape(iso639_code, phonetic_config, phonetic_file)
+
+    # Remove files for languages that failed to be scraped in 4 tries.
+    if phonemic_count == None or phonetic_count == None:
+      print(f"TOO MANY RETRIES ON {iso639_code} MOVING ON TO NEXT.")
+      os.remove(iso639_code + ".tsv")
+      os.remove(iso639_code + "_phonetic" + ".tsv")
+      continue
+    # Remove files for languages that failed to call scrape
+    elif phonemic_count == 0 and phonetic_count == 0:
+      print(f"FAILED TO CALL SCRAPE ON {iso639_code}.")
+      os.remove(iso639_code + ".tsv")
+      os.remove(iso639_code + "_phonetic" + ".tsv")
+      continue      
+
 
     if phonemic_count >= phonetic_count:
       # Will create link to non-existent tsv file when config call fails - as with 'yue'
@@ -33,6 +63,7 @@ def main():
     else:
       row = [f"[TSV File]({iso639_code}_phonetic.tsv)"] + row
       row.extend(["Phonetic", str(phonetic_count)])
+
 
     readme_string = "| " + " | ".join(row) + " |\n"
     readme_file.write(readme_string)
--- languages/wikipron/api_test/readme_test.md
+++ languages/wikipron/api_test/readme_test.md
@@ -1,6 +1,3 @@
 
 | Link | ISO 639-2 Code | ISO 639 Language Name | Wiktionary Language Name | Case-folding | Phonetic/Phonemic | # of entries |
 | :---- | :----: | :----: | :----: | :----:| :----: | ----: |
-| [TSV File](baq.tsv) | baq | Basque | Basque | True | Phonemic | 219 |
-| [TSV File](cor_phonetic.tsv) | cor | Cornish | Cornish | True | Phonetic | 403 |
-| [TSV File](dlm.tsv) | dlm | Dalmatian | Dalmatian | True | Phonemic | 176 |
