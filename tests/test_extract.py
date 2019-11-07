1fbcd813a03486613d44950736cc3bb80a16a8c6
jacksonllee <jacksonlunlee@gmail.com>
2019-11-06 23:40:23 -0600

    A more strict test for annotations, not just for args

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,14 +1,18 @@
-import inspect
-
 import pytest
+import requests
 
 from wikipron.extract import EXTRACTION_FUNCTIONS
 
 
 @pytest.mark.parametrize("func", EXTRACTION_FUNCTIONS.values())
 def test_extraction_functions_have_the_same_signature(func):
-    expected_args = ["word", "request", "config"]
-    actual_args = list(inspect.signature(func).parameters.keys())
-    assert expected_args == actual_args, (
+    expected_annotations = {
+        "word": "Word",
+        "request": requests.Response,
+        "config": "Config",
+        "return": "Iterator[WordPronPair]",
+    }
+    actual_annotations = func.__annotations__
+    assert expected_annotations == actual_annotations, (
         f"{func.__qualname__} does not have the expected function signature.",
     )
