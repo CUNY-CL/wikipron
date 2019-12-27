756d2076b043cc95440bf404802491b03c2790ca
Lucas Ashby <lfeashby@gmail.com>
2019-12-27 09:21:36 -0700

    Minor testing of generalized split

--- languages/wikipron/src/generalized_split.py
+++ languages/wikipron/src/generalized_split.py
@@ -2,6 +2,9 @@ import json
 import regex
 import sys
 
+# TODO - Add removal of non-split files. Hopefully that
+# doesn't lead to problems with the iteration in the bash script.
+
 # ../tsv/yid_phonetic.tsv
 tsv_path = sys.argv[1]
 
@@ -12,11 +15,15 @@ with open("languages.json", "r") as lang_source:
 def generalized_check(script, word):
     script = "{" + script + "}"
     regex_string = r"^\p{script}+$".format(script=script)
-    # print(regex_string)
     return bool(regex.match(regex_string, word))
 
 
+# Hopefully kanji entries just get filtered out.
+
+
 def iterate_through_file(tsv_path, unicode_script, path):
+    # This appears to retain sorted order. Not sure if that is
+    # guaranteed though.
     with open(tsv_path, "r") as source:
         with open(path, "w") as destination:
             for line in source:
@@ -26,10 +33,8 @@ def iterate_through_file(tsv_path, unicode_script, path):
 
 
 def check_file():
-    iso639_code = tsv_path[tsv_path.rindex("/") + 1: tsv_path.index("_")]
-    transcription_level = tsv_path[
-        tsv_path.rindex("_") + 1:
-    ]
+    iso639_code = tsv_path[tsv_path.rindex("/") + 1 : tsv_path.index("_")]
+    transcription_level = tsv_path[tsv_path.rindex("_") + 1 :]
 
     if "script" in languages[iso639_code]:
         if len(languages[iso639_code]["script"]) > 1:
@@ -41,9 +46,8 @@ def check_file():
                     # Then this is a previously split file.
                     return
 
-            # print("REMAINING", lang)
             for script_prefix, unicode_script in lang["script"].items():
-                output_path = f"../tsv/{iso639_code}_{script_prefix}_{transcription_level}.tsv"
+                output_path = f"../tsv/{iso639_code}_{script_prefix}_{transcription_level}"
                 iterate_through_file(tsv_path, unicode_script, output_path)
 
 
