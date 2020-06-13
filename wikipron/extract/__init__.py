b0e774a1abe54414576aeb2d27241f65c95999ae
Lucas Ashby <lfeashby@gmail.com>
2020-06-13 15:48:25 -0400

    Vietnamese extraction function (#181)
    * first pass at adding functionality for not skipping spaces in ortho/prons
    
    * fixes to scrape.py - adds filtering of languages dictionary down to wikipron accepted keys
    
    * reformatted with white and added typing
    
    * add skipping on pronunciations with non-break space character, adds no_skip_spaces_pron flag for tibetan and persian
    
    * adds non-break space char handling for orthography
    
    * updates changelog
    
    * updates skip_word and skip_pron tests
    
    * fixes cli test failure, adds pr number to changelog
    
    * changes flag name, updates src/scrape config settings creation
    
    * remove logging statement in cli.py
    
    * highly unrefined attempt at fixing vietnamese
    
    * adds additional dialects to vietnamese
    
    * raw wikipron scrape for vietnamese
    
    * removed duplicates and updated README summary
    
    * simplified vietnamese extraction function
    
    * remove old vietnamese files before rescrping
    
    * vietnamese scrape
    
    * postprocessing and summary generation for vietnamese with dialects
    
    * updated changelog, resolved tests
    
    * adds typing

--- wikipron/extract/__init__.py
+++ wikipron/extract/__init__.py
@@ -3,6 +3,7 @@ from wikipron.extract.jpn import extract_word_pron_jpn
 from wikipron.extract.khm import extract_word_pron_khmer
 from wikipron.extract.lat import extract_word_pron_latin
 from wikipron.extract.tha import extract_word_pron_thai
+from wikipron.extract.vie import extract_word_pron_vie
 
 
 # All extraction functions must have the exact same function signature.
@@ -13,4 +14,5 @@ EXTRACTION_FUNCTIONS = {
     "Khmer": extract_word_pron_khmer,
     "Latin": extract_word_pron_latin,
     "Thai": extract_word_pron_thai,
+    "Vietnamese": extract_word_pron_vie,
 }
