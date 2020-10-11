8ce5cd47ba8b7937fdc6d092c774102bfb4e807b
Lucas Ashby <lfeashby@gmail.com>
2020-10-10 20:18:19 -0400

    basic framework for testing file creation involved in big scrape

--- tests/test_data/test_affixation.py
+++ tests/test_data/test_affixation.py
@@ -1,19 +1,75 @@
+import os
+import shutil
 
 import pytest
 
 from data.src.scrape import _build_scraping_config
 
-# "mul" is code given to Translingual
+
+PARENT_DIR = os.path.dirname(os.getcwd())
+
+# "mul" is code given to Translingual by Wiktionary.
+# Resolved to "Multiple Languages" by iso639 package.
 @pytest.mark.parametrize(
-    "config_settings, dialect_suffix, expected_file_name",
+    "config_settings, dialect_suffix, phones, expected_file_name",
     [
-        ({"key": "mul"}, "test_", "mul_test_phonetic.tsv"),
+        # Dialect and phones
+        (
+            {"key": "mul"},
+            "test_",
+            True,
+            [
+                "mul_test_phonetic.tsv",
+                "mul_test_phonemic.tsv",
+                "mul_test_phonetic_filtered.tsv",
+                "mul_test_phonemic_filtered.tsv",
+            ],
+        ),
+        # Dialect
+        (
+            {"key": "mul"},
+            "test_",
+            False,
+            [
+                "mul_test_phonetic.tsv",
+                "mul_test_phonemic.tsv",
+            ],
+        ),
+        # Standard
+        (
+            {"key": "mul"},
+            "",
+            False,
+            [
+                "mul_phonetic.tsv",
+                "mul_phonemic.tsv",
+            ],
+        ),
     ],
 )
-def test_something(config_settings, dialect_suffix, expected_file_name):
-    # Create a spoof tsv directory
+def test_something(config_settings, dialect_suffix, phones, expected_file_name):
+    tsv_path = f"{PARENT_DIR}/tsv"
+    phones_path = f"{PARENT_DIR}/phones"
+    os.mkdir(tsv_path)
+    os.mkdir(phones_path)
+
+    if phones:
+        with open(
+            f"{phones_path}/{config_settings['key']}_{dialect_suffix}phonetic.phones",
+            "w",
+        ) as f1:
+            f1.write("a")
+        with open(
+            f"{phones_path}/{config_settings['key']}_{dialect_suffix}phonemic.phones",
+            "w",
+        ) as f2:
+            f2.write("a")
     _build_scraping_config(
         config_settings=config_settings, dialect_suffix=dialect_suffix
     )
-    # Inspect contents of spoof tsv directory
-    # Delete spoof tsv directory
+    contents = os.listdir(tsv_path)
+    shutil.rmtree(tsv_path)
+    shutil.rmtree(phones_path)
+
+    for produced_file in contents:
+        assert produced_file in expected_file_name
