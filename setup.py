dbe5418d8c7705c05935bd6a990ebdbdcafc10cf
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2020-10-16 06:33:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ def main():
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Development Status :: 3 - Alpha",
             "Environment :: Console",
             "License :: OSI Approved :: Apache Software License",
