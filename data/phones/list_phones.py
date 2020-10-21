473df450b9849117a70d307dae6b76938c5bfee8
jacksonllee <jacksonlunlee@gmail.com>
2020-10-21 10:19:40 -0500

    DOC update docstrings and readme

--- data/phones/list_phones.py
+++ data/phones/list_phones.py
@@ -15,18 +15,14 @@ import random
 from typing import Dict, List, Set
 
 
-def _get_cli_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("filepath", help="Path to TSV scraped by WikiPron")
-    args = parser.parse_args()
-    return args
-
-
 def _count_phones(filepath: str) -> Dict[str, Set[str]]:
-    # phone_to_examples as Dict[str, Set[str]] is the most straightforward
-    # data structure for our purposes. It's not memory-efficient
-    # (with the same word-pron pair appearing in different phones' sets),
-    # but anything fancier doesn't seem worth the work.
+    """Count the phones in the given TSV file.
+
+    phone_to_examples as Dict[str, Set[str]] is the most straightforward
+    data structure for our purposes. It's not memory-efficient
+    (with the same word-pron pair appearing in different phones' sets),
+    but anything fancier doesn't seem worth the work.
+    """
     phone_to_examples = collections.defaultdict(set)
     with open(filepath, encoding="utf-8") as f:
         for line in f:
@@ -42,9 +38,12 @@ def _count_phones(filepath: str) -> Dict[str, Set[str]]:
 
 
 def _pick_examples_for_display(examples: Set[str]) -> List[str]:
-    # We could have exposed the maximum number of examples to display
-    # (set to be 3 now) to the command-line interface,
-    # but it doesn't seem worth it for the time being.
+    """Pick examples of word-pron pairs for display.
+
+    We could have exposed the maximum number of examples to display
+    (set to be 3 now) for each phone to the command-line interface,
+    but it doesn't seem worth it for the time being.
+    """
     n_examples = min(len(examples), 3)
     # Using list() here because Python 3.9 has deprecated the use
     # of an _unordered_ set as the input to random.sample.
@@ -52,14 +51,14 @@ def _pick_examples_for_display(examples: Set[str]) -> List[str]:
     return sample
 
 
-def main():
-    args = _get_cli_args()
+def main(args: argparse.Namespace):
     phone_to_examples: Dict[str, Set[str]] = _count_phones(args.filepath)
     for phone, examples in sorted(
         phone_to_examples.items(), key=lambda x: len(x[1]), reverse=True
     ):
         print(
             f"{phone:<5}"
+            " # "
             f"{len(examples):<10}"
             f"{', '.join(_pick_examples_for_display(examples))}"
         )
@@ -67,4 +66,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("filepath", help="Path to TSV scraped by WikiPron")
+    args = parser.parse_args()
+    main(args)
