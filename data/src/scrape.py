c5afa9523c6295eedbbde2f6b02c8653094d115e
Lucas Ashby <lfeashby@gmail.com>
2021-01-25 12:38:45 -0500

    revert comment in scrape.py

--- data/src/scrape.py
+++ data/src/scrape.py
@@ -166,7 +166,6 @@ def main(args: argparse.Namespace) -> None:
             exit(1)
         codes = list(rset)
 
-    # 2021-01-08
     # "2020-01-15" (Big Scrape 3).
     cut_off_date = datetime.date.today().isoformat()
     wikipron_accepted_settings = {
