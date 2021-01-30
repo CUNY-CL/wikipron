7e3b85f6bb0a5b2794d7bad13a5c0248f3500d40
Alexander Gutkin <agutkin@google.com>
2021-01-30 18:42:27 +0000

    Fixed annotation errors.

--- data/src/split.py
+++ data/src/split.py
@@ -8,7 +8,7 @@ import unicodedataplus
 
 import regex  # type: ignore
 
-from typing import Tuple
+from typing import Dict, Tuple, Union
 
 from data.src.codes import LANGUAGES_PATH, TSV_DIRECTORY_PATH
 
@@ -23,19 +23,19 @@ def _generalized_check(script: str, word: str) -> bool:
 
 def _detect_best_script_name(
     word: str, strict: bool = True
-) -> Tuple[str, float]:
+) -> Union[Tuple[str, float], None]:
     """Returns the most likely script name (rather than ISO 15924 code) the
     word belongs to along with the corresponding confidence expressed as a
     maximum likelihood estimate computed over the `word` sample. If `strict`
     is enabled, then all the characters must belong to the same script and
-    `None` is returned on failure.
+    `(None, None)` is returned on failure.
 
     Example: "ژۇرنال" -> ("Arabic", 1.0).
     """
-    script_probs = collections.defaultdict(float)
+    script_counts: Dict[str, float] = collections.defaultdict(float)
     for char in word:
-        script_probs[unicodedataplus.script(char)] += 1.0
-    script_probs = [(s, script_probs[s] / len(word)) for s in script_probs]
+        script_counts[unicodedataplus.script(char)] += 1.0
+    script_probs = [(s, script_counts[s] / len(word)) for s in script_counts]
     script_probs = sorted(script_probs, key=lambda x: x[1], reverse=True)
     if strict and len(script_probs) != 1:
         return None
