4b1b78d0035780b7e328ec49e49cc78ec37b9ad5
Alexander Gutkin <agutkin@google.com>
2021-02-05 07:16:27 +0000

    Added Western Lawa language code.

--- wikipron/languagecodes.py
+++ wikipron/languagecodes.py
@@ -431,4 +431,6 @@ LANGUAGE_CODES = {
     # South Levantine Arabic. ISO 639-3 only.
     "ajp": "South Levantine Arabic",
     "south levantine arabic": "South Levantine Arabic",
+    # Western Lawa: ISO 639-3 only.
+    "lcp": "Western Lawa",
 }
