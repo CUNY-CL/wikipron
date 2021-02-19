b44842295e28078b7f7c34603fe63adc9ac1d2cf
Arundhati Sengupta <ciil.sengupta@gmail.com>
2021-02-19 17:17:39 -0500

    Addressing review comments shared by KG

--- data/src/error_analysis/erroranalysis.py
+++ data/src/error_analysis/erroranalysis.py
@@ -1,70 +1,77 @@
-"""Error Analysis tool for G2P.
+"""Error analysis tool for G2P.
 
-This script assumes 2 input files. a) Covering Grammer b) Test output. 
-CG file  : Each line contain graphime and their corresponding pronunciation seperated by a tab
-Test File: contains three attributes in each line seperated by a tab. The attributes are - orthography, expected pronunciation, and hypothesized pronunciation."""
+This script assumes 2 input files. a) covering grammar b) test output.
+CG file: Each line contain grapheme and their corresponding pronunciation
+separated by a tab
+Test file: contains three attributes in each line separated by a tab.
+Example: espresso ɛ s p ɹ ɛ s ə ʊ     ɛ k s p ɹ ɛ s ə ʊ
+ """
 
 __author__ = "Arundhati Sengupta"
 
 import pynini
 import argparse
+from prettytable import PrettyTable
 
-def main(args: argparse.Namespace):
+
+def main(args: argparse.Namespace) -> None:
     cg_fst = pynini.string_file(args.cg_path).closure().optimize()
-    rulematch_predMatch = 0
-    rulematch_predNotMatch = 0
-    notRulematch_predMatch = 0
-    notRulematch_predNotMatch = 0
+    rulematch_predmatch = 0
+    rulematch_pred_notmatch = 0
+    not_rulematch_predmatch = 0
+    not_rulematch_pred_notmatch = 0
     total_records = 0
     with open(args.test_path, "r") as source:
         for line in source:
-            total_records+=1
-            parts = line.split('\t')
+            total_records += 1
+            parts = line.split("\t")
             lg = parts[0].strip()
-            act = parts[1].replace(' ','').replace('.','').strip()
-            predPron = parts[2].replace(' ','').replace('.','').strip()
-             
-            lattice = (lg @ cg_fst @ predPron).project(True)
-            
+            act = parts[1].replace(" ", "").replace(".", "").strip()
+            pred_pron = parts[2].replace(" ", "").replace(".", "").strip()
+            lattice = (lg @ cg_fst @ pred_pron).project(True)
             if lattice.start() == pynini.NO_STATE_ID:
-                if (act == predPron):
-                    notRulematch_predMatch += 1
-                    
+                if act == pred_pron:
+                    not_rulematch_predmatch += 1
                 else:
-                    notRulematch_predNotMatch += 1            
+                    not_rulematch_pred_notmatch += 1
             else:
-                if (act == predPron):
-                    rulematch_predMatch += 1
+                if act == pred_pron:
+                    rulematch_predmatch += 1
                 else:
-                    rulematch_predNotMatch += 1
-    
-    print ('Total Number of Records', total_records)
-    ruleMpredNM = round(rulematch_predNotMatch / total_records, 4) * 100
-    ruleMpredM = round(rulematch_predMatch / total_records, 4) * 100
-    ruleNMpredM = round (notRulematch_predMatch / total_records, 4) * 100
-    ruleNMpredNM = round(notRulematch_predNotMatch / total_records, 4) * 100
+                    rulematch_pred_notmatch += 1
 
-    printtable(ruleMpredNM, ruleMpredM,ruleNMpredM,ruleNMpredNM)
+    print("Total Number of Records", total_records)
+    rule_m_pred_nm = (rulematch_pred_notmatch / total_records) * 100
+    rule_m_pred_m = (rulematch_predmatch / total_records) * 100
+    rule_nm_pred_m = (not_rulematch_predmatch / total_records) * 100
+    rule_nm_pred_nm = (not_rulematch_pred_notmatch / total_records) * 100
+
+    printtable(rule_m_pred_nm, rule_m_pred_m, rule_nm_pred_m, rule_nm_pred_nm)
 
 
-def printtable(ruleMpredNM, ruleMpredM,ruleNMpredM,ruleNMpredNM):
-    ruleMpredNM = '{:05.2f}'.format(ruleMpredNM)
-    ruleMpredM = '{:05.2f}'.format(ruleMpredM)
-    ruleNMpredM = '{:05.2f}'.format(ruleNMpredM)
-    ruleNMpredNM = '{:05.2f}'.format(ruleNMpredNM)
+def printtable(rule_m_pred_nm, rule_m_pred_m, rule_nm_pred_m, rule_nm_pred_nm):
+    rule_m_pred_nm = "{:05.2f}".format(rule_m_pred_nm)
+    rule_m_pred_m = "{:05.2f}".format(rule_m_pred_m)
+    rule_nm_pred_m = "{:05.2f}".format(rule_nm_pred_m)
+    rule_nm_pred_nm = "{:05.2f}".format(rule_nm_pred_nm)
 
-    print('                                            ')
-    print('               | CG Match  |   CG Not Match |')
-    print('---------------|-----------+----------------|')
-    print('Pron Match     | ',ruleMpredM,'   |     ',ruleNMpredM,'    |')
-    print('Pron Not Match | ',ruleMpredNM,'   |     ',ruleNMpredNM,'    |')
+    # printing the result in a table
+    x = PrettyTable()
+
+    x.field_names = ["", "CG Match", "CG Not Match"]
+
+    x.add_row(["Pron Match", rule_m_pred_m, rule_nm_pred_m])
+    x.add_row(["Pron Not Match", rule_m_pred_nm, rule_nm_pred_nm])
+
+    print(x)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        "--cg_path", required=True, help="path to file of covering grammar which is a tsv file. Each line contain graphime and their corresponding pronunciation seperated by a tab"
+        "--cg_path", required=True, help="path to TSV covering grammar file"
     )
     parser.add_argument(
-        "--test_path", required=True, help="path to test tsv file which contains three attributes in each line seperated by a tab. The attributes are - orthography, expected pronunciation, and hypothesized pronunciation."
+        "--test_path", required=True, help="path to test tsv file"
     )
     main(parser.parse_args())
