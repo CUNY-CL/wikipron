3a3a167282f2c61967d299553eaccb7bb2d65c2a
reubenraff <reubenraff@gmail.com>
2021-03-18 11:20:14 -0400

    adds unimorph data repo and download routine
    adds the json file with all unimorph files and wikipron lg names. Additionally uses download routine to grab data and logs statements to the console

--- data/morphology/grab_unimorph_data.py
+++ data/morphology/grab_unimorph_data.py
@@ -6,47 +6,82 @@ import logging
 import os
 import requests
 import time
-
+start_time = time.time()
 from typing import Dict
+
+
 
 
 UNIMORPH_DICT_PATH = "unimorph_languages.json"
 
 
-def download(data_to_grab: Dict[str, str]) -> Dict[str, str]:
-    to_retry = {}
-    os.mkdir("tsv")
-    for language, url in data_to_grab.items():
-        with requests.get(url, stream=True) as response:
-            logging.info("Downloading: %s", language)
+def download():
+    #start_time = time.time()
+
+    os.makedirs("unimorph_for_split", exist_ok=True)
+    with open("unimorph_languages.json") as jfile:
+        data_to_grab = json.load(jfile)
+    #print(data_to_grab)
+    urls = list(data_to_grab.values())
+    #print(urls)
+    #sprint(urls)
+    lg_codes = list(data_to_grab.keys())
+    #print(data_to_grab["glg"])
+
+    for i in range(len(urls)):
+        with requests.get(urls[i], stream=True) as response:
+            target_path = urls[i].split("/")[-1]
+            #print(target_path)
+
+            logging.info("Downloading: %s", target_path)
             if response.status_code == 200:
-                with open(f"tsv/{language}.tsv", "wb") as f:
-                    f.write(response.content)
+
+                #unimorph/fao/master
+                with open(f"unimorph_for_split/{target_path}.tsv", "w+") as sink:
+                    #print(response.text)
+                    print(response.text,file=sink)
+            elif target_path == "glg":
+                with requests.get(data_to_grab["glg"], stream=True) as response:
+                    logging.info("Downloading: %s", target_path)
+                    if response.status_code == 200:
+                        with open(f"unimorph_for_split/glg.tsv", "w+") as sink:
+                            print(response.text,file=sink)
+
+                        #unimorph/fao/master
+                        #with open(f"unimorph_for_split/gal.tsv", "w+") as sink:
+                            #print(response.text)
+                            #print(response.text,file=sink)
+                    elif target_path == "geo":
+                        with requests.get(data_to_grab["glg"], stream=True) as response:
+                            logging.info("Downloading: %s", target_path)
+                            if response.status_code == 200:
+                                with open(f"unimorph_for_split/glg.tsv", "w+") as sink:
+                                    print(response.text,file=sink)
+
             else:
                 logging.info(
-                    "Status code %d while downloading %s",
-                    response.status_code,
-                    language,
+                "Status code %s while downloading %s",
+                response.status_code,
+                target_path
                 )
-                to_retry[language] = data_to_grab[language]
-        # 30 seconds appears to not be enough, 60-70 seconds works well
-        # but takes a long time.
-        time.sleep(45)
-    return to_retry
+
+            time.sleep(45)
+#download()
 
 
 def main() -> None:
-    with open(UNIMORPH_DICT_PATH, "r", encoding="utf-8") as langs:
-        languages = json.load(langs)
+    #with open(UNIMORPH_DICT_PATH, "r", encoding="utf-8") as langs:
+    #    languages = json.load("unimorph_languages.json")
     # Hack for repeatedly attempting to download Wortschatz data
     # as a way of getting around 404 response from their server.
-    langs_to_retry = download(languages)
-    while langs_to_retry:
-        langs_to_retry = download(langs_to_retry)
-
+    #langs_to_retry = download(languages)
+    #while langs_to_retry:
+    #    langs_to_retry = download(langs_to_retry)
+    download()
 
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(filename)s %(levelname)s: %(message)s", level="INFO"
     )
+    print("--- %s seconds ---" % (time.time() - start_time))
     main()
