709c37c144a18ff3ff8149a18b7c217870f0f4a5
reubenraff <reubenraff@gmail.com>
2021-03-18 17:01:10 -0400

    Update grab_unimorph_data.py

--- data/morphology/grab_unimorph_data.py
+++ data/morphology/grab_unimorph_data.py
@@ -16,27 +16,19 @@ UNIMORPH_DICT_PATH = "unimorph_languages.json"
 
 
 def download():
-    #start_time = time.time()
-
     os.makedirs("unimorph_for_split", exist_ok=True)
     with open("unimorph_languages.json") as jfile:
         data_to_grab = json.load(jfile)
-    #print(data_to_grab)
-    urls = list(data_to_grab.values())
-    #print(urls)
-    #sprint(urls)
-    lg_codes = list(data_to_grab.keys())
-    #print(data_to_grab["glg"])
 
+    urls = list(data_to_grab.values())
+    lg_codes = list(data_to_grab.keys())
+
+    
     for i in range(len(urls)):
         with requests.get(urls[i], stream=True) as response:
             target_path = urls[i].split("/")[-1]
-            #print(target_path)
-
             logging.info("Downloading: %s", target_path)
             if response.status_code == 200:
-
-                #unimorph/fao/master
                 with open(f"unimorph_for_split/{target_path}.tsv", "w+") as sink:
                     #print(response.text)
                     print(response.text,file=sink)
@@ -46,11 +38,6 @@ def download():
                     if response.status_code == 200:
                         with open(f"unimorph_for_split/glg.tsv", "w+") as sink:
                             print(response.text,file=sink)
-
-                        #unimorph/fao/master
-                        #with open(f"unimorph_for_split/gal.tsv", "w+") as sink:
-                            #print(response.text)
-                            #print(response.text,file=sink)
                     elif target_path == "geo":
                         with requests.get(data_to_grab["glg"], stream=True) as response:
                             logging.info("Downloading: %s", target_path)
@@ -74,9 +61,9 @@ def main() -> None:
     #    languages = json.load("unimorph_languages.json")
     # Hack for repeatedly attempting to download Wortschatz data
     # as a way of getting around 404 response from their server.
-    #langs_to_retry = download(languages)
-    #while langs_to_retry:
-    #    langs_to_retry = download(langs_to_retry)
+    langs_to_retry = download()
+    while langs_to_retry:
+        langs_to_retry = download()
     download()
 
 if __name__ == "__main__":
