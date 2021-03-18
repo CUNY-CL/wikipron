8d1042c39e1ad8a4b485cb1d3272a280b7b87d64
reubenraff <reubenraff@gmail.com>
2021-03-18 17:08:00 -0400

    Update grab_unimorph_data.py
    fixes regression errors from previous version, adds new import block for third party library requests, properly formats spacing.

--- data/morphology/grab_unimorph_data.py
+++ data/morphology/grab_unimorph_data.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Downloads UniMorph morphological paradigms data."""
-
 import json
 import logging
 import os
@@ -9,11 +8,11 @@ from typing import Dict
 
 
 """ import requests in separate code block because of third party status """
-
 import requests
 
 
 start_time = time.time()
+
 
 UNIMORPH_DICT_PATH = "unimorph_languages.json"
 
@@ -22,11 +21,8 @@ def download():
     os.makedirs("unimorph_for_split", exist_ok=True)
     with open(UNIMORPH_DICT_PATH) as jfile:
         data_to_grab = json.load(jfile)
-
     urls = list(data_to_grab.values())
     lg_codes = list(data_to_grab.keys())
-
-    
     for i in range(len(urls)):
         with requests.get(urls[i], stream=True) as response:
             target_path = urls[i].split("/")[-1]
@@ -46,16 +42,13 @@ def download():
                             if response.status_code == 200:
                                 with open(f"unimorph_for_split/glg.tsv", "w+") as sink:
                                     print(response.text,file=sink)
-
             else:
                 logging.info(
                 "Status code %s while downloading %s",
                 response.status_code,
                 target_path
                 )
-
             time.sleep(45)
-#download()
 
 
 def main() -> None:
@@ -66,7 +59,7 @@ def main() -> None:
     langs_to_retry = download()
     while langs_to_retry:
         langs_to_retry = download()
-    download()
+    
 
 if __name__ == "__main__":
     logging.basicConfig(
