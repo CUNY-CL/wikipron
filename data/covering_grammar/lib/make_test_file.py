e8e965be0ba468a00e94fdf564e8921d10ca64bf
Kyle Gorman <kylebgorman@gmail.com>
2021-04-13 07:45:20 -0400

    Uses context manager for file closing. (#399)
    * Uses context manager for file closing.
    
    Also:
    
    * tightens up docs
    * makes mismatch a fatal error
    * removes unnecessary date formatting (we don't need this because
    logging isn't logging the time)
    
    * changelog

--- data/covering_grammar/lib/make_test_file.py
+++ data/covering_grammar/lib/make_test_file.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python
 """Makes test file.
 
-Takes the gold data and the model output, and creates a three-column TSV where
-each line has a word, its gold pronunciation, and the predicted pronunciation.
-Assumes that the input files have the same words in the same order.
-"""
+Using gold data and the model output, this script creates a three-column TSV
+file in which each row contains a word, its gold pronunciation, and the
+predicted pronunciation, assuming that the input files have the words listed
+in the same order."""
 
 import argparse
+import contextlib
 import logging
 
 from data.scrape.lib.codes import LOGGING_PATH
 
 
 def main(args: argparse.Namespace) -> None:
-    with open(args.gold, "r") as gf, open(args.pred, "r") as pf:
-        with open(args.out, "w") as wf:
-            for lineno, (g_line, p_line) in enumerate(zip(gf, pf), 1):
-                # Note that we use `strip` to remove the newline.
-                g_word, g_pron = g_line.rstrip().split("\t")
-                p_word, p_pron = p_line.rstrip().split("\t")
-                # Make sure that gold data and predictions have the
-                # same words.
-                if g_word != p_word:
-                    logging.warning(
-                        "%s != %s (line %d)", g_word, p_word, lineno
-                    )
-                    continue
-                print(f"{g_word}\t{g_pron}\t{p_pron}", file=wf)
+    with contextlib.ExitStack() as stack:
+        gf = stack.enter_context(open(args.gold, "r"))
+        pf = stack.enter_context(open(args.pred, "r"))
+        wf = stack.enter_context(open(args.out, "w"))
+        for lineno, (g_line, p_line) in enumerate(zip(gf, pf), 1):
+            g_word, g_pron = g_line.rstrip().split("\t", 2)
+            p_word, p_pron = p_line.rstrip().split("\t", 2)
+            # Ensures the gold data and predictions have the same words.
+            if g_word != p_word:
+                logging.error("%s != %s (line %d)", g_word, p_word, lineno)
+                exit(1)
+            print(f"{g_word}\t{g_pron}\t{p_pron}", file=wf)
 
 
 if __name__ == "__main__":
@@ -36,7 +35,6 @@ if __name__ == "__main__":
             logging.FileHandler(LOGGING_PATH, mode="a"),
             logging.StreamHandler(),
         ],
-        datefmt="%d-%b-%y %H:%M:%S",
         level="INFO",
     )
     parser = argparse.ArgumentParser(description=__doc__)
@@ -46,5 +44,5 @@ if __name__ == "__main__":
     parser.add_argument(
         "pred", help="TSV with words and predicted pronunciations"
     )
-    parser.add_argument("out", help="file to write to")
+    parser.add_argument("out", help="output file")
     main(parser.parse_args())
