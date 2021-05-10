83d3b176f795a8958d6764024959ffe10fb0be4d
Yeonju Lee-Sikka <yeonju123@gmail.com>
2021-05-09 22:20:39 -0400

    added writing an error log

--- data/covering_grammar/lib/error_analysis.py
+++ data/covering_grammar/lib/error_analysis.py
@@ -18,10 +18,28 @@ __author__ = "Arundhati Sengupta"
 
 
 import argparse
+import csv
+import datetime
+import os
 
+import errno
 import prettytable  # type: ignore
 import pynini
 from pynini.lib import rewrite
+
+
+def create_new_log_dir() -> str:
+    error_log_dir = os.path.join(os.getcwd(), "logs")
+    try:
+         os.makedirs(error_log_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise  Exception("Failed to create a log directory")
+    return error_log_dir
+
+
+def get_current_timestamp():
+    return datetime.datetime.now().strftime("%m%d%Y_%H%M")
 
 
 def match_pronunciation_rule(ortho, pron, cg_fst):
@@ -39,21 +57,28 @@ def main(args: argparse.Namespace) -> None:
         not_rulematch_predmatch = 0
         not_rulematch_pred_notmatch = 0
         total_records = 0
+        error_log_dir = create_new_log_dir()    #create error_log directory using current timestamp
+        today_timestamp = get_current_timestamp()
         with open(args.test_path, "r") as source:
-            for line in source:
-                total_records += 1
-                ortho, gold_p, hypo_p = line.rstrip().split("\t", 2)
-                hypo_p = hypo_p.replace(" ", "")
-                gold_p = gold_p.replace(" ", "")
-                if match_pronunciation_rule(ortho, hypo_p, cg_fst):
-                    if gold_p == hypo_p:
-                        rulematch_predmatch += 1
+            with open(os.path.join(error_log_dir, today_timestamp + "_error.tsv"), "w", encoding = "utf8") as f:
+                writer = csv.writer(f)
+                writer.writerow(["Orthography", "Gold" ,"Hypo"])
+                for line in source:
+                    total_records += 1
+                    ortho, gold_p, hypo_p = line.rstrip().split("\t", 2)
+                    hypo_p = hypo_p.replace(" ", "")
+                    gold_p = gold_p.replace(" ", "")
+                    if match_pronunciation_rule(ortho, hypo_p, cg_fst):
+                        if gold_p == hypo_p:
+                            rulematch_predmatch += 1
+                        else:
+                            rulematch_pred_notmatch += 1
+                            writer.writerow([ortho, gold_p, hypo_p])
+                    elif gold_p == hypo_p:
+                        not_rulematch_predmatch += 1
                     else:
-                        rulematch_pred_notmatch += 1
-                elif gold_p == hypo_p:
-                    not_rulematch_predmatch += 1
-                else:
-                    not_rulematch_pred_notmatch += 1
+                        not_rulematch_pred_notmatch += 1
+                        writer.writerow([ortho, gold_p, hypo_p])
         # Collects percentages.
         rule_m_pred_nm = 100 * rulematch_pred_notmatch / total_records
         rule_m_pred_m = 100 * rulematch_predmatch / total_records
