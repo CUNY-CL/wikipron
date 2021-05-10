a544d0ad6c47157410e45bb9ce218cbec9217383
Yeonju Lee-Sikka <yeonju123@gmail.com>
2021-05-09 23:27:02 -0400

    improve codes

--- data/covering_grammar/lib/error_analysis.py
+++ data/covering_grammar/lib/error_analysis.py
@@ -22,24 +22,19 @@ import csv
 import datetime
 import os
 
-import errno
 import prettytable  # type: ignore
 import pynini
 from pynini.lib import rewrite
 
 
-def create_new_log_dir() -> str:
-    error_log_dir = os.path.join(os.getcwd(), "logs")
-    try:
-        os.makedirs(error_log_dir)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise Exception("Failed to create a log directory")
-    return error_log_dir
-
-
 def get_current_timestamp():
     return datetime.datetime.now().strftime("%m%d%Y_%H%M")
+
+
+def log() -> str:
+    error_log_dir = os.path.join(os.getcwd(), "logs")
+    os.makedirs(error_log_dir, exist_ok=True)
+    return error_log_dir
 
 
 def match_pronunciation_rule(ortho, pron, cg_fst):
@@ -57,18 +52,22 @@ def main(args: argparse.Namespace) -> None:
         not_rulematch_predmatch = 0
         not_rulematch_pred_notmatch = 0
         total_records = 0
-        error_log_dir = (
-            create_new_log_dir()
-        )  # create error_log directory using current timestamp
+        error_log_dir = log()
         today_timestamp = get_current_timestamp()
         with open(args.test_path, "r") as source:
             with open(
-                os.path.join(error_log_dir, today_timestamp + "_error.tsv"),
+                os.path.join(error_log_dir, today_timestamp + ".log"),
                 "w",
                 encoding="utf8",
-            ) as f:
-                writer = csv.writer(f)
-                writer.writerow(["Orthography", "Gold", "Hypo"])
+            ) as log_file:
+                fieldnames = ["Orthography", "Gold", "Hypo"]
+                tsv_writer_object = csv.DictWriter(
+                    log_file,
+                    fieldnames=fieldnames,
+                    delimiter="\t",
+                    lineterminator="\n",
+                )
+                tsv_writer_object.writeheader()
                 for line in source:
                     total_records += 1
                     ortho, gold_p, hypo_p = line.rstrip().split("\t", 2)
@@ -79,12 +78,24 @@ def main(args: argparse.Namespace) -> None:
                             rulematch_predmatch += 1
                         else:
                             rulematch_pred_notmatch += 1
-                            writer.writerow([ortho, gold_p, hypo_p])
+                            tsv_writer_object.writerow(
+                                {
+                                    "Orthography": ortho,
+                                    "Gold": gold_p,
+                                    "Hypo": hypo_p,
+                                }
+                            )
                     elif gold_p == hypo_p:
                         not_rulematch_predmatch += 1
                     else:
                         not_rulematch_pred_notmatch += 1
-                        writer.writerow([ortho, gold_p, hypo_p])
+                        tsv_writer_object.writerow(
+                            {
+                                "Orthography": ortho,
+                                "Gold": gold_p,
+                                "Hypo": hypo_p,
+                            }
+                        )
         # Collects percentages.
         rule_m_pred_nm = 100 * rulematch_pred_notmatch / total_records
         rule_m_pred_m = 100 * rulematch_predmatch / total_records
