67573af011ba9ba81d7e04a245242642a93bf18e
jacksonllee <jacksonlunlee@gmail.com>
2022-02-01 21:09:44 -0500

    ENH grab updated frequencies

--- data/frequencies/grab_wortschatz_data.py
+++ data/frequencies/grab_wortschatz_data.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """Downloads and decompresses Wortschatz frequency data."""
 
+import argparse
 import json
 import logging
 import os
+import re
 import tarfile
 import time
 
@@ -15,10 +17,24 @@ import requests
 WORTSCHATZ_DICT_PATH = "wortschatz_languages.json"
 
 
-def download(data_to_grab: Dict[str, Any]) -> Dict[str, Any]:
+def download(data_to_grab: Dict[str, Any], args: argparse.Namespace) -> Dict[str, Any]:
     to_retry = {}
-    os.mkdir("tgz")
+    os.makedirs("tgz", exist_ok=True)
+    codes = frozenset(key[:3] for key in data_to_grab)
+    if args.restriction:
+        restriction_set = frozenset(
+            re.split(r"[;,\s]+\s*", args.restriction.strip(";, "))
+        )
+        if len(restriction_set) == 1 and not list(restriction_set)[0]:
+            raise ValueError("Restriction flag raised but no language provided")
+        if not restriction_set.issubset(codes):
+            for key in restriction_set - codes:
+                raise ValueError(f"{key} is not a valid ISO code", key)
+    else:
+        restriction_set = codes
     for language in data_to_grab:
+        if language[:3] not in restriction_set:
+            continue
         url = data_to_grab[language]["data_url"]
         with requests.get(url, stream=True) as response:
             target_path = url.split("/")[-1]
@@ -40,7 +56,7 @@ def download(data_to_grab: Dict[str, Any]) -> Dict[str, Any]:
 
 
 def unpack() -> None:
-    os.mkdir("tsv")
+    os.makedirs("tsv", exist_ok=True)
     for tarball in os.listdir("tgz"):
         logging.info("Unpacking: %s", tarball)
         with tarfile.open(name=f"tgz/{tarball}", mode="r:gz") as tar_data:
@@ -52,14 +68,14 @@ def unpack() -> None:
                     tar_data.extract(file_entry, "tsv")
 
 
-def main() -> None:
+def main(args: argparse.Namespace) -> None:
     with open(WORTSCHATZ_DICT_PATH, "r", encoding="utf-8") as langs:
         languages = json.load(langs)
     # Hack for repeatedly attempting to download Wortschatz data
     # as a way of getting around 404 response from their server.
-    langs_to_retry = download(languages)
+    langs_to_retry = download(languages, args)
     while langs_to_retry:
-        langs_to_retry = download(langs_to_retry)
+        langs_to_retry = download(langs_to_retry, args)
     unpack()
 
 
@@ -67,4 +83,10 @@ if __name__ == "__main__":
     logging.basicConfig(
         format="%(filename)s %(levelname)s: %(message)s", level="INFO"
     )
-    main()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--restriction",
+        type=str,
+        help="restricts download to specified language(s)",
+    )
+    main(parser.parse_args())
