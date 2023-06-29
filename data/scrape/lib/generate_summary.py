9589e49d73b939449245c4cae33d96dc97b54557
Biatris <43286928+Biatris@users.noreply.github.com>
2023-06-29 16:36:57 -0400

    Added KPI code to generate_summary.py

--- data/scrape/lib/generate_summary.py
+++ data/scrape/lib/generate_summary.py
@@ -5,10 +5,10 @@ import json
 import logging
 import operator
 import os
-
+import pandas
 from typing import Any, Dict
 
-from data.scrape.lib.codes import (
+from codes import (
     LANGUAGES_PATH,
     README_PATH,
     LANGUAGES_SUMMARY_PATH,
@@ -26,9 +26,9 @@ def _handle_modifiers(
         end = file_path.rindex("_broad") + 1
     else:
         end = file_path.rindex("_narrow") + 1
-    script_key = file_path[start : file_path.index("_", start)]
+    script_key = file_path[start: file_path.index("_", start)]
     dialect_key = file_path[
-        file_path.index("_", start) + 1 : file_path.rindex("_", start, end)
+        file_path.index("_", start) + 1: file_path.rindex("_", start, end)
     ]
     script = language["script"][script_key]
     dialect = dialects.get(dialect_key, "").replace(" |", ",")
@@ -41,9 +41,7 @@ def main() -> None:
     readme_list = []
     summaries = []
     for file_path in os.listdir(TSV_DIRECTORY):
-        with open(
-            f"{TSV_DIRECTORY}/{file_path}", "r", encoding="utf-8"
-        ) as tsv:
+        with open(f"{TSV_DIRECTORY}/{file_path}", "r", encoding="utf-8") as tsv:
             num_of_entries = sum(1 for line in tsv)
         # Removes files with less than 100 entries.
         if num_of_entries < 100:
@@ -114,10 +112,69 @@ def main() -> None:
                 f"| {count:,} |",
                 file=sink,
             )
+    # adding KPI code
+    # Currently hard-coded; what's a good alternative?
+    TSV_PATH = "../tsv_summary.tsv"
+    table = pandas.read_csv(
+        TSV_PATH,
+        sep="\t",
+        names=[
+            "link",
+            "iso_639_2_code",
+            "iso_639_language_name",
+            "wiktionary_language_name",
+            "script",
+            "dialect",
+            "filtered",
+            "narrow_broad",
+            "case_folding",
+            "no_entries",
+        ],
+    )
+    # Somewhat arbitrarily, we use the ISO 639-2 code as the "real" label for
+    # languages.
+    languages = table.iso_639_2_code.nunique()
+    print(f"Languages:\t{languages:,}")
+    broad_files = table["narrow_broad"].value_counts()["Broad"]
+    narrow_files = table["narrow_broad"].value_counts()["Narrow"]
+    # We don't currently have any case where multiple languages have a
+    # "southern" vs. a "northern" dialect, but just in case, we use this
+    # slightly more complex logic here.
+    dialects = table.groupby(["iso_639_2_code", "dialect"]).ngroups
+    print(f"Dialects:\t{dialects:,}")
+    df = table[table["dialect"] != ""]
+    grouped_counts = (
+        df.groupby(["iso_639_2_code", "dialect", "narrow_broad"])
+        .size()
+        .unstack(fill_value=0)
+    )
+    narrow_pairs_files = grouped_counts["Narrow"].sum()
+    broad_pairs_files = grouped_counts["Broad"].sum()
+    scripts = table.script.nunique()
+    print(f"Scripts:\t{scripts:,}")
+    # We only count "unfiltered" pronunciations, since filtered ones are a
+    # strict subset thereof. Note that this gives us a smaller number than
+    # previously reported, but it seems more correct.
+    entries = table[~table.filtered].no_entries.sum()
+    print(f"Entries:\t{entries:,}")
+
+    bullet_list = (  
+        f"* Languages: {languages} \n"
+        f"\t* Broad transcription files: {broad_files} \n"
+        f"\t* Narrow transcription files: {narrow_files} \n"
+        f"* Languages & dialects: {dialects} \n"
+        f"\t* Broad transcription files: {broad_pairs_files} \n"
+        f"\t* Narrow transcription files: {narrow_pairs_files} \n"
+        f"* Scripts: {scripts} \n"
+        f"* Pronunciations: {entries:,}\n\n"
+    )
+    with open(README_PATH, "r") as original:
+        data = original.read()
+    with open(README_PATH, "w") as modified:
+        modified.write(bullet_list + data)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(filename)s %(levelname)s: %(message)s", level="INFO"
-    )
+    logging.basicConfig(format="%(filename)s %(levelname)s: %(message)s", level="INFO")
     main()
+
