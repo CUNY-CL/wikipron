ec652aace0f6a4ec115125781c6542d8b165a477
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2024-07-11 09:11:37 -0700

    Fix dialect xpath selector

--- src/wikipron/config.py
+++ src/wikipron/config.py
@@ -30,7 +30,7 @@ _PRON_XPATH_SELECTOR_TEMPLATE = """
 """
 _DIALECT_XPATH_SELECTOR_TEMPLATE = (
     "and\n"
-    '  (span[@class = "ib-content" and a[{dialects_text}]]\n'
+    '  (span[@class = "ib-content"]//a[{dialects_text}]\n'
     '   or count(span[@class = "ib-content"]) = 0)'
 )
 _PHONEMES_REGEX = r"/(.+?)/"
